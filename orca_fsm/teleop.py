6c4f475af44232084a185f57ad20578a9bfda96e
bbnoir <bbnoir83@gmail.com>
2024-03-23 16:40:20 +0800

    feat: change move strategy

--- orca_fsm/teleop.py
+++ orca_fsm/teleop.py
@@ -7,6 +7,7 @@ import termios
 import tty
 import threading
 from std_msgs.msg import Float32MultiArray
+from rclpy.executors import MultiThreadedExecutor
 
 
 class KeyboardController(Node):
@@ -16,21 +17,32 @@ class KeyboardController(Node):
 
         self.dx = 0.0
         self.dy = 0.0
-        self.dz = 0.0
-        self.yaw = 0.0
+        self.depth = 0.1
         self.target_yaw = 0.0
-        # arm control 0: idle, 1: drop, 2: stretch out, 3: stretch in
-        self.arm_state = 0.0
+        self.drop = 0.0
+        self.arm_mode = 0.0
 
         self.LIN_VEL_STEP = 1.0
-        self.ANG_VEL_STEP = 1.0
+        self.DEP_VEL_STEP = 0.1
+        self.ANG_VEL_STEP = 10.0
 
         self.settings = termios.tcgetattr(sys.stdin)
         self.key_timeout = 0.5
 
         self.pub = self.create_publisher(
             Float32MultiArray,
-            'fsm_output',
+            'target',
+            10)
+
+        self.real_yaw = 0.0
+        self.real_arm_done = 0.0
+        self.real_depth = 0.0
+
+        # 0: yaw, 1: arm, 2: depth
+        self.sub = self.create_subscription(
+            Float32MultiArray,
+            'rpi_to_oring',
+            self.callback,
             10)
         
     def get_key(self):
@@ -48,79 +60,68 @@ class KeyboardController(Node):
         if key == 'w':
             self.dx = self.LIN_VEL_STEP
             self.dy = 0.0
-            self.dz = 0.0
-            self.target_yaw = self.yaw
             self.get_logger().info("front")
         elif key == 'a':
             self.dx = 0.0
             self.dy = -self.LIN_VEL_STEP
-            self.dz = 0.0
-            self.target_yaw = self.yaw
             self.get_logger().info("left")
         elif key == 's':
             self.dx = -self.LIN_VEL_STEP
             self.dy = 0.0
-            self.dz = 0.0
-            self.target_yaw = self.yaw
             self.get_logger().info("back")
         elif key == 'd':
             self.dx = 0.0
             self.dy = self.LIN_VEL_STEP
-            self.dz = 0.0
-            self.target_yaw = self.yaw
             self.get_logger().info("right")
         elif key == 'k':
-            self.dx = 0.0
-            self.dy = 0.0
-            self.dz = self.LIN_VEL_STEP
-            self.target_yaw = self.yaw
-            self.get_logger().info("up")
+            if (self.depth - self.DEP_VEL_STEP) >= 0.0:
+                self.depth -= self.DEP_VEL_STEP
+                self.get_logger().info("up: depth = %f" % self.depth)
         elif key == 'j':
-            self.dx = 0.0
-            self.dy = 0.0
-            self.dz = -self.LIN_VEL_STEP
-            self.target_yaw = self.yaw
-            self.get_logger().info("down")
+            self.depth += self.DEP_VEL_STEP
+            self.get_logger().info("down: depth = %f" % self.depth)
         elif key == 'h':
-            self.dx = 0.0
-            self.dy = 0.0
-            self.dz = 0.0
-            self.target_yaw = self.yaw + self.ANG_VEL_STEP * 90.0
-            self.get_logger().info("yaw_left")
+            self.target_yaw -= self.ANG_VEL_STEP
+            self.get_logger().info("turn_left: yaw = %f" % self.target_yaw)
         elif key == 'l':
-            self.dx = 0.0
-            self.dy = 0.0
-            self.dz = 0.0
-            self.target_yaw = self.yaw - self.ANG_VEL_STEP * 90.0
-            self.get_logger().info("yaw_right")
+            self.target_yaw += self.ANG_VEL_STEP
+            self.get_logger().info("turn_right: yaw = %f" % self.target_yaw)
         elif key == 'q':
             self.dx = 0.0
             self.dy = 0.0
-            self.dz = 0.0
-            self.target_yaw = self.yaw
             self.get_logger().info("stop")
         elif key == 'u':
-            self.arm_state = 0.0
-            self.get_logger().info("idle")
+            if (self.drop == 1.0):
+                self.drop = 0.0
+                self.get_logger().info("drop close")
+            else:
+                self.drop = 1.0
+                self.get_logger().info("drop open")
         elif key == 'i':
-            self.arm_state = 1.0
-            self.get_logger().info("drop")
+            self.arm_mode = 0.0
+            self.get_logger().info("arm idle")
         elif key == 'o':
-            self.arm_state = 2.0
-            self.get_logger().info("stretch out")
+            self.arm_mode = 1.0
+            self.get_logger().info("arm grab")
         elif key == 'p':
-            self.arm_state = 3.0
-            self.get_logger().info("stretch in")
+            self.arm_mode = 2.0
+            self.get_logger().info("arm release")
 
         msg = Float32MultiArray()
-        msg.data = [self.dx, self.dy, self.dz, self.target_yaw, self.arm_state]
+        msg.data = [self.dx, self.dy, self.depth, self.target_yaw, self.drop, self.arm_mode]
         self.pub.publish(msg)
+
+    def callback(self, msg):
+        self.real_yaw = msg.data[0]
+        self.real_arm_done = msg.data[1]
+        self.real_depth = msg.data[2]
 
 def main():
     rclpy.init()
     node = KeyboardController()
+    executor = MultiThreadedExecutor()
 
-    spin_thread = threading.Thread(target=rclpy.spin, args=(node,))
+    spin_thread = threading.Thread(target=rclpy.spin, args=(node, executor,))
     spin_thread.start()
 
     rate = node.create_rate(5)
